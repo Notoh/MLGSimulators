6e0adcbf2490e6c76551e15f425c9f132d2037ac
Alex Pawelko <hisaysajp@gmail.com>
2017-07-01 17:18:06 -0600

    Java version

---
+++ java/src/main/java/io/notoh/mlgsim/Dorito.java
@@ -0,0 +1,8 @@
+package io.notoh.mlgsim;
+
+public class Dorito {
+
+    public static final int PTS_TURN = 10;
+    public static final int COST = 30;
+
+}
---
+++ java/src/main/java/io/notoh/mlgsim/MtnDew.java
@@ -0,0 +1,8 @@
+package io.notoh.mlgsim;
+
+public class MtnDew {
+
+    public static final int PTS_TURN = 20;
+    public static final int COST = 60;
+
+}
